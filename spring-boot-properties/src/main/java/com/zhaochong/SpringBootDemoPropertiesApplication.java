e61a57a3c4dbd41097db2eb9f9f6620d627c88cf
zhaochong2 <zhaochong2@yy.com>
2020-04-14 14:10:53 +0800

    [add] 增加配置文件模块读取

---
+++ spring-boot-properties/src/main/java/com/zhaochong/SpringBootDemoPropertiesApplication.java
@@ -0,0 +1,18 @@
+package com.zhaochong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description TODO
+ * @Author 赵冲
+ * @Email zhaochong2@yy.com
+ * @Date ${DATE} ${TIME}
+ */
+@SpringBootApplication
+public class SpringBootDemoPropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoPropertiesApplication.class, args);
+	}
+}
