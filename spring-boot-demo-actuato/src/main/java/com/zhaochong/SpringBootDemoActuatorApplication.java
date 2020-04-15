6bba1d75ec3909e62a46bb430400102e8879e1df
zhaochong2 <zhaochong2@yy.com>
2020-04-15 14:01:54 +0800

    [add ] spring-boot 集成 spring-boot-starter-actuator 用于监控 spring-boot 的启动和运行状态

---
+++ spring-boot-demo-actuato/src/main/java/com/zhaochong/SpringBootDemoActuatorApplication.java
@@ -0,0 +1,18 @@
+package com.zhaochong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description TODO
+ * @Author 赵冲
+ * @Email zhaochong2@yy.com
+ * @Date ${DATE} ${TIME}
+ */
+@SpringBootApplication
+public class SpringBootDemoActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoActuatorApplication.class, args);
+	}
+}
