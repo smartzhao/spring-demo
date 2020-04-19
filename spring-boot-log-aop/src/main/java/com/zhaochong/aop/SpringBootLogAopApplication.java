c0b1c740ed8f439314c8861a6354ea4ddebb7e21
zhaochong2 <zhaochong2@yy.com>
2020-04-19 18:12:24 +0800

    [add ] 	spring-boot 使用 AOP 切面的方式记录 web 请求日志

---
+++ spring-boot-log-aop/src/main/java/com/zhaochong/aop/SpringBootLogAopApplication.java
@@ -0,0 +1,11 @@
+package com.zhaochong.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLogAopApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLogAopApplication.class, args);
+    }
+}
