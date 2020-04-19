1cf558b912bd55f1f4d84c9970b85b2515fa1644
zhaochong2 <zhaochong2@yy.com>
2020-04-19 21:17:28 +0800

    [add ] spring-boot 集成 Freemarker 模板引擎

---
+++ spring-boot-demo-template-freemarker/src/main/java/com/zhaochong/SpringBootDemoTemplateFreemarkerApplication.java
@@ -0,0 +1,11 @@
+package com.zhaochong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoTemplateFreemarkerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoTemplateFreemarkerApplication.class, args);
+    }
+}
---
+++ spring-boot-demo-template-freemarker/src/main/java/com/zhaochong/modle/modle/User.java
@@ -0,0 +1,14 @@
+package com.zhaochong.modle.modle;
+
+import lombok.Data;
+
+/**
+ * <p>
+ * 用户 model
+ * </p>
+ */
+@Data
+public class User {
+	private String name;
+	private String password;
+}
