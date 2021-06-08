4f172b17642ac519b99459c35b328fe39c7b203f
zhaochong <zoro.zhaochong@gmail.com>
2021-06-08 21:10:37 +0800

    fix: [spring-boot-hello] clean  code.

---
+++ spring-boot-helloworld/src/main/java/com/zhaochong/SpringBootDemoHelloWorldApplication.java
@@ -0,0 +1,25 @@
+package com.zhaochong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * SpringBoot启动类
+ * </p>
+ *
+ * @package: com.zhaochong
+ * @description: SpringBoot启动类
+ * @author: zhaochong
+ * @date:
+ * @copyright: Copyright (c)
+ * @version: V1.0
+ * @modified: zhaochong
+ */
+@SpringBootApplication
+public class SpringBootDemoHelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoHelloWorldApplication.class, args);
+    }
+}
---
+++ spring-boot-helloworld/src/main/java/com/zhaochong/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.zhaochong.controller;
+
+import cn.hutool.core.util.StrUtil;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    /**
+     * Hello，World
+     *
+     * @param who 参数，非必须
+     * @return Hello, ${who}
+     */
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(required = false, name = "who") String who) {
+        if (StrUtil.isBlank(who)) {
+            who = "Spring boot 2";
+        }
+        return StrUtil.format("Hello {}", who);
+    }
+}
